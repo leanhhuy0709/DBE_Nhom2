518683129bfd3dd307fa009f5b0b2256b0c424fa
leanhhuy0709 <78292068+leanhhuy0709@users.noreply.github.com>
2022-12-09 19:20:24 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,25 +11,45 @@ import NavigationBar from './component/NavigationBar/NavigationBar'
 import User from './page/User/User'
 
 import React from 'react';
-import Group from './page/Group/Group'
+import VerticalBar from './component/VerticalBar/VerticalBar'
 
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
 
+import VerticalBar from './component/VerticalBar/VerticalBar';
+
 function App() {
+  const user = {"username": "huy123", "fname": "Huy", "mname": "Anh", "lname": "Le", "position": "Designer"};
+  const projects = [{"id": 1, "name": "ABC", "description": "lorem ipsum", "cost_efficiency": -30, "cost": 10000, "leader": {}, "model": [{}], "supplier": [{}], "group": [{}]},
+  {"id": 1, "name": "ABC", "description": "lorem ipsum", "cost_efficiency": -30, "cost": 10000, "leader": {}, "model": [{}], "supplier": [{}], "group": [{}]},
+  {"id": 1, "name": "ABC", "description": "lorem ipsum", "cost_efficiency": -30, "cost": 10000, "leader": {}, "model": [{}], "supplier": [{}], "group": [{}]},
+  {"id": 1, "name": "ABC", "description": "lorem ipsum", "cost_efficiency": -30, "cost": 10000, "leader": {}, "model": [{}], "supplier": [{}], "group": [{}]},
+];
+  const activity = [{}];
+
+
+  if(localStorage.getItem('usr'))
   return (
     <BrowserRouter>
-      <NavigationBar/>
+      <NavigationBar user = {user}/>
+      <VerticalBar/>
       <Routes>
+        
         <Route path = '/' element = {<Home/>}/>
-        <Route path = '/login' element = {<Login/>}/>
-        <Route path = '/project' element = {<Project/>}/>
+        <Route path = '/project' element = {<Project projects = {projects}/>}/>
         <Route path = '/activity' element = {<Activity/>}/>
         <Route path = '/user' element = {<User/>}/>
         
       </Routes>
     </BrowserRouter>
   );
+  else
+  return (<BrowserRouter>
+    <Routes>
+
+      <Route path = '/*' element = {<Login/>}/>
+      
+    </Routes>
+  </BrowserRouter>);
 }
 
 export default App;
-//<Route path = '/group' element = {<Group/>}/>
