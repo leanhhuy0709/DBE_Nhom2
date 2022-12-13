7bfedf6c2c753fcc89d424fc9078882e50f4e6f1
trannquanggthangg <80170226+trannquanggthangg@users.noreply.github.com>
2022-12-13 18:58:58 +0700

    Update proceduce.sql
    sua ham get_model_of_worker, them ham insert_comment, insert_model

--- proceduce.sql
+++ proceduce.sql
@@ -1,6 +1,7 @@
+
 -- Show Employee List
-DELIMITER // 
 DROP PROCEDURE IF EXISTS ShowEmployee;
+DELIMITER // 
 CREATE PROCEDURE ShowEmployee()
 BEGIN
 SELECT * FROM EmployeeView;
@@ -9,8 +10,8 @@ END //
 DELIMITER ;
 
 -- Show Project List
-DELIMITER //
 DROP PROCEDURE IF EXISTS ShowProject;
+DELIMITER //
 CREATE PROCEDURE ShowProject()
 BEGIN
 SELECT * FROM ProjectView;
@@ -19,9 +20,9 @@ END //
 DELIMITER ;
 
 -- Show Product List
-DELIMITER //
 DROP PROCEDURE IF EXISTS ShowProduct;
-CREATE PROCEDURE ShowProject()
+DELIMITER //
+CREATE PROCEDURE ShowProduct()
 BEGIN
 SELECT * FROM ProductView;
 END //
@@ -29,8 +30,8 @@ END //
 DELIMITER ;
 
 -- Show Activity List
-DELIMITER //
 DROP PROCEDURE IF EXISTS ShowActivity;
+DELIMITER //
 CREATE PROCEDURE ShowActivity()
 BEGIN
 SELECT * FROM ActivityView;
@@ -39,8 +40,8 @@ END //
 DELIMITER ;
 
 -- Show Model List
-DELIMITER //
 DROP PROCEDURE IF EXISTS ShowModel;
+DELIMITER //
 CREATE PROCEDURE ShowModel()
 BEGIN
 SELECT * FROM ModelView;
@@ -49,8 +50,8 @@ END //
 DELIMITER ;
 
 -- Show Info of Employee with input: ID
-DELIMITER //
 DROP PROCEDURE IF EXISTS ViewInfo;
+DELIMITER //
 CREATE PROCEDURE ViewInfo(IN id_ int)
 BEGIN
 DROP TABLE IF EXISTS temp_info;
@@ -63,8 +64,8 @@ DELIMITER ;
 
 -- login --
 -- username and password entered by the user -- 
-DELIMITER //
 DROP PROCEDURE IF EXISTS Login;
+DELIMITER //
 CREATE PROCEDURE Login (
 	IN username_ VARCHAR(20),
     IN password_ VARCHAR(100)
@@ -77,8 +78,8 @@ END //
 DELIMITER ;
 
 -- Remove
-DELIMITER //
 DROP PROCEDURE IF EXISTS RemoveEmployee;
+DELIMITER //
 CREATE PROCEDURE RemoveEmployee (IN id_ int)
 remove_employee: BEGIN
     DELETE FROM Employee
@@ -86,8 +87,8 @@ remove_employee: BEGIN
 END //
 DELIMITER ;
 
-DELIMITER //
 DROP PROCEDURE IF EXISTS RemoveProject;
+DELIMITER //
 CREATE PROCEDURE RemoveProject (IN pid_ int)
 remove_project: BEGIN
     DELETE FROM Project
@@ -95,8 +96,8 @@ remove_project: BEGIN
 END //
 DELIMITER ;
 
-DELIMITER //
 DROP PROCEDURE IF EXISTS RemoveProduct;
+DELIMITER //
 CREATE PROCEDURE RemoveProduct (IN pid_ int)
 remove_product: BEGIN
     DELETE FROM Product
@@ -104,8 +105,8 @@ remove_product: BEGIN
 END //
 DELIMITER ;
 
-DELIMITER //
 DROP PROCEDURE IF EXISTS RemoveActivity;
+DELIMITER //
 CREATE PROCEDURE RemoveActivity (IN aid_ int)
 remove_activity: BEGIN
     DELETE FROM Activity
@@ -114,8 +115,8 @@ END //
 DELIMITER ;
 
 -- update info 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS update_info;
+DELIMITER \\
 CREATE PROCEDURE update_info(IN id_ int, IN ssn_ VARCHAR(10), IN fname_ VARCHAR(50),
 							 IN mname_ VARCHAR(50),  IN lname_ VARCHAR(50), IN bdate_ DATE,
 							 IN addr_ VARCHAR(200), IN salary_ FLOAT, IN type_ int)
@@ -133,8 +134,8 @@ CREATE PROCEDURE update_info(IN id_ int, IN ssn_ VARCHAR(10), IN fname_ VARCHAR(
     END \\
 DELIMITER ;
 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS update_pass;
+DELIMITER \\
 CREATE PROCEDURE update_pass(IN username_ VARCHAR(20),
                              IN password_ VARCHAR(100))
 	BEGIN 
@@ -147,8 +148,8 @@ DELIMITER ;
 
 
 -- Insert
-DELIMITER \\
 DROP PROCEDURE IF EXISTS insert_employee;
+DELIMITER \\
 CREATE PROCEDURE insert_employee(IN ssn_ VARCHAR(10), IN fname_ VARCHAR(50),
 							 IN mname_ VARCHAR(50),  IN lname_ VARCHAR(50), IN bdate_ DATE,
 							 IN addr_ VARCHAR(200), IN salary_ FLOAT, IN type_ int)
@@ -160,19 +161,19 @@ CREATE PROCEDURE insert_employee(IN ssn_ VARCHAR(10), IN fname_ VARCHAR(50),
     END \\
 DELIMITER ;
 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS insert_project;
-CREATE PROCEDURE insert_project(IN name_ VARCHAR(200), IN description_ VARCHAR(500), IN cost_efficiency_ VARCHAR(100), IN cost_ FLOAT)
+DELIMITER \\
+CREATE PROCEDURE insert_project(IN id int,IN name_ VARCHAR(200), IN description_ VARCHAR(500), IN cost_efficiency_ VARCHAR(100), IN cost_ FLOAT)
 	BEGIN
 		DECLARE pid_ int;
 		SET pid_ = (SELECT IFNULL(MAX(PID)+1, 1) FROM Project ORDER BY PID ASC);
-		INSERT INTO Project
-		VALUES (pid_, name_, description_, cost_efficiency_, cost_);
+		INSERT INTO Project(PID, ID, Name, Description, CostEfficiency, Cost, StartDate, Period)
+		VALUES (pid_, id ,name_, description_, cost_efficiency_, cost_, NOW(), 'fisrt');
     END \\
 DELIMITER ;
 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS insert_product;
+DELIMITER \\
 CREATE PROCEDURE insert_product(IN name_ VARCHAR(200), IN description_ VARCHAR(500), IN cost_efficiency_ VARCHAR(100), IN cost_ FLOAT)
 	BEGIN
 		DECLARE pid_ int;
@@ -183,34 +184,22 @@ CREATE PROCEDURE insert_product(IN name_ VARCHAR(200), IN description_ VARCHAR(5
 DELIMITER ;
 
 
--- -- cần 
--- DELIMITER \\
--- DROP PROCEDURE IF EXISTS insert_activity;
--- CREATE PROCEDURE insert_activity()
--- 	BEGIN
--- 		DECLARE pid_ int;
--- 		SET pid_ = (SELECT IFNULL(MAX(PID)+1, 1) FROM Project ORDER BY PID ASC);
--- 		INSERT INTO Project
--- 		VALUES (pid_, name_, description_, cost_efficiency_, cost_);
---     END \\
--- DELIMITER ;
-
 
 -- lấy group thuộc về project
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_group_of_project;
+DELIMITER \\
 CREATE PROCEDURE get_group_of_project(IN pid_ int)
 	BEGIN
 		select GNumber, Name, Location
 		from `Group`
-		where Group.PID = pid_;
+		where `Group`.PID = pid_;
     END \\
 DELIMITER ;
 
 
 -- lấy leader của project
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_leader_of_project;
+DELIMITER \\
 CREATE PROCEDURE get_leader_of_project(IN pid_ int)
 	BEGIN
 		select Employee.ID, FName, MName, LName, StartDate, Period, EmployeeType 
@@ -221,8 +210,8 @@ DELIMITER ;
 
 
 -- lấy model của project
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_model_of_project;
+DELIMITER \\
 CREATE PROCEDURE get_model_of_project(IN pid_ int)
 	BEGIN
 		select Model.MNumber as Mnumber, DateCreated, Project.PID as ProjectID, Model.ID  as DesignerID
@@ -233,8 +222,8 @@ DELIMITER ;
 
 
 -- lấy supplier của project
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_supplier_of_project;
+DELIMITER \\
 CREATE PROCEDURE get_supplier_of_project(IN pid_ int)
 	BEGIN
 		select SID, Location, Date, Quantity, Price 
@@ -243,22 +232,8 @@ CREATE PROCEDURE get_supplier_of_project(IN pid_ int)
     END \\
 DELIMITER ;
 
--- -- lấy part của project
--- DELIMITER \\
--- DROP PROCEDURE IF EXISTS get_part_of_project;
--- CREATE PROCEDURE get_supplier_of_project(IN pid_ int)
--- 	BEGIN
--- 		select SID, Location, Date, Quantity, Price 
--- 		from Supplies, Supplier
--- 		where Supplies.SupplierID = Supplier.SID and Supplies.ProjectID = pid_;
---     END \\
--- DELIMITER ;
-
--- Thêm Employee vào Group
--- id của employee và (gnumber,pid) của group
--- cần có trigger kiểm tra
-DELIMITER \\
 DROP PROCEDURE IF EXISTS add_employee_to_group;
+DELIMITER \\
 CREATE PROCEDURE add_employee_to_group(in id_ int, in gnumber_ int, in pid_ int)
 	BEGIN
 		insert into Joins 
@@ -267,8 +242,8 @@ CREATE PROCEDURE add_employee_to_group(in id_ int, in gnumber_ int, in pid_ int)
 DELIMITER ;
 
 -- lấy danh sách employee có trong group
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_employee_of_group;
+DELIMITER \\
 CREATE PROCEDURE get_employee_of_group(in gnumber_ int, in pid_ int)
 	BEGIN
 		select Employee.ID as ID, SSN, FName, MName, LName, BDate, Address, Salary, EmployeeType
@@ -278,8 +253,8 @@ CREATE PROCEDURE get_employee_of_group(in gnumber_ int, in pid_ int)
 DELIMITER ;
 
 --
-DELIMITER \\
 DROP PROCEDURE IF EXISTS insert_model;
+DELIMITER \\
 CREATE PROCEDURE insert_model(In Mnumber int, in PID int, in ID int, in DateCreated date)
 	BEGIN
 		insert into Model
@@ -288,8 +263,8 @@ CREATE PROCEDURE insert_model(In Mnumber int, in PID int, in ID int, in DateCrea
 DELIMITER ;
 
 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS delete_model;
+DELIMITER \\
 CREATE PROCEDURE delete_model(In Mnumber_ int, in PID_ int, in ID_ int)
 	BEGIN
 		delete from Model
@@ -299,8 +274,8 @@ DELIMITER ;
 
 -- Thêm model vào project
 -- (MNumber, PID, ID) của model và ProjectID của project
-DELIMITER \\
 DROP PROCEDURE IF EXISTS add_model_to_project;
+DELIMITER \\
 CREATE PROCEDURE add_model_to_project(In Mnumber_ int, in PID_ int, in ID_ int, in ProjectID_ int)
 	BEGIN
 		insert into ModelInProject 
@@ -308,8 +283,8 @@ CREATE PROCEDURE add_model_to_project(In Mnumber_ int, in PID_ int, in ID_ int, 
     END \\
 DELIMITER ;
 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS notifications_of_equipment;
+DELIMITER \\
 CREATE PROCEDURE notifications_of_equipment()
 	BEGIN
 		select Equipment.PID as EquipmentID, AComment as Comment
@@ -320,8 +295,8 @@ DELIMITER ;
 
 
 
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_tasks_of_group;
+DELIMITER \\
 CREATE PROCEDURE get_tasks_of_group(in GNumber int, in PID int)
 	BEGIN
 		select TNumber, Name
@@ -331,45 +306,34 @@ CREATE PROCEDURE get_tasks_of_group(in GNumber int, in PID int)
 DELIMITER ;
 
 -- lấy danh sách group mà employee tham gia
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_group_of_employee;
+DELIMITER \\
 CREATE PROCEDURE get_group_of_employee(in EmployeeID int)
 	BEGIN
-		select Group.GNumber, Group.PID, Name, Location
+		select `Group`.GNumber, `Group`.PID, Name, Location
 		from `Group`, Joins
-		where Group.GNumber =Joins.GNumber and Group.PID =Joins.PID and Joins.ID=EmployeeID;
+		where `Group`.GNumber =Joins.GNumber and `Group`.PID =Joins.PID and Joins.ID=EmployeeID;
     END \\
 DELIMITER ;
 
 
 -- lấy danh sách project mà employee tham gia
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_project_of_employee;
+DELIMITER \\
 CREATE PROCEDURE get_project_of_employee(in EmployeeID int)
 	BEGIN
 		select Project.PID, Project.Name, Project.Description, Project.CostEfficiency, 
 				Project.Cost, Project.ID, Project.StartDate, Project.Period
 		from Project,Joins,`Group`
-		where Group.GNumber=Joins.GNumber and Group.PID=Joins.PID 
-				and Joins.ID=EmployeeID and Project.PID=Group.PID;
+		where `Group`.GNumber=Joins.GNumber and `Group`.PID=Joins.PID 
+				and Joins.ID=EmployeeID and Project.PID=`Group`.PID;
     END \\
 DELIMITER ;
 
--- CREATE TABLE Employee (
--- 	ID INTEGER PRIMARY KEY,
--- 	SSN VARCHAR(10) UNIQUE NOT NULL,
--- 	FName VARCHAR(50),
--- 	MName VARCHAR(50),
--- 	LName VARCHAR(50),
--- 	BDate DATE,
--- 	Address VARCHAR(200),
--- 	Salary FLOAT CHECK (Salary>=0),
--- 	EmployeeType enum('Employee','Analyst','Manager','Designer','Worker') default 'Employee'
--- );
 
 -- lấy danh sách model của designer
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_model_of_designer;
+DELIMITER \\
 CREATE PROCEDURE get_model_of_designer(in DesignerID int)
 	BEGIN
 		select MNumber,PID,ID,DateCreated
@@ -378,19 +342,10 @@ CREATE PROCEDURE get_model_of_designer(in DesignerID int)
     END \\
 DELIMITER ;
 
--- CREATE TABLE Model (
--- 	MNumber INTEGER,
--- 	PID INTEGER,
--- 	ID INTEGER,
--- 	DateCreated DATE,
--- 	PRIMARY KEY (MNumber, PID, ID),
--- 	FOREIGN KEY (PID) REFERENCES Product(PID) ON DELETE CASCADE ON UPDATE CASCADE,
--- 	FOREIGN KEY (ID) REFERENCES Designer(ID) ON DELETE CASCADE ON UPDATE CASCADE
--- );
 
 -- lấy danh sách product của designer
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_product_of_designer;
+DELIMITER \\
 CREATE PROCEDURE get_product_of_designer(in DesignerID int)
 	BEGIN
 		select Model.PID as ProductID
@@ -401,8 +356,8 @@ DELIMITER ;
 
 
 -- lấy danh sách product mà worker làm việc
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_product_of_worker;
+DELIMITER \\
 CREATE PROCEDURE get_product_of_worker(in WorkerID int)
 	BEGIN
 		select Produces.ProductID as ProductID 
@@ -411,43 +366,47 @@ CREATE PROCEDURE get_product_of_worker(in WorkerID int)
     END \\
 DELIMITER ;
 
--- CREATE TABLE Produces (
--- 	ProductID INTEGER,
--- 	EquipmentID INTEGER,
--- 	ProcessID INTEGER NOT NULL,
--- 	Status VARCHAR(20) CHECK (Status IN ('Success', 'Failure', 'Undefined')),
--- 	Date DATE,
--- 	Hour TIMESTAMP,
--- 	PRIMARY KEY (ProductID, EquipmentID),
--- 	UNIQUE (ProductID, ProcessID),
--- 	FOREIGN KEY (ProductID) REFERENCES Product(PID) ON DELETE CASCADE ON UPDATE CASCADE,
--- 	FOREIGN KEY (EquipmentID) REFERENCES Equipment (PID) ON DELETE CASCADE ON UPDATE CASCADE,
--- 	FOREIGN KEY (ProcessID) REFERENCES Process (PID) ON DELETE CASCADE ON UPDATE CASCADE
--- );
 
 -- lấy danh sách equipment của worker
-DELIMITER \\
 DROP PROCEDURE IF EXISTS get_equipment_of_worker;
+DELIMITER \\
 CREATE PROCEDURE get_equipment_of_worker(in WorkerID int)
 	BEGIN
-		select PID, ASession
+		select Works_on.PID, works_on.ASession
 		from Works_on, wSession
 		where Works_on.ID=WorkerID and wSession.ID=Works_on.ID and wSession.PID=Works_on.PID;
     END \\
 DELIMITER ;
 
--- CREATE TABLE Works_on (
--- 	ID INTEGER,
--- 	PID INTEGER,
--- 	PRIMARY KEY (ID, PID),
--- 	FOREIGN KEY (ID) REFERENCES Worker(ID) ON DELETE CASCADE ON UPDATE CASCADE,
--- 	FOREIGN KEY (PID) REFERENCES Equipment(PID) ON DELETE CASCADE ON UPDATE CASCADE
--- );
+-- thêm model
+DROP PROCEDURE IF EXISTS insert_model;
+DELIMITER \\
+CREATE PROCEDURE insert_model(in PID int, in ID int)
+	BEGIN
+		DECLARE MNumber int;
+		SET MNumber = (SELECT IFNULL(MAX(Model.MNumber)+1, 1)
+						FROM Model 
+						where Model.PID=PID and Model.ID=ID);
+		insert into Model
+		values(MNumber,PID,ID,current_date());
+    END \\
+DEL
 
--- CREATE TABLE wSession (
--- 	ID INTEGER,
--- 	PID INTEGER,
--- 	ASession VARCHAR(20),
--- 	PRIMARY KEY (ID, PID, ASession),
--- 	FOREIGN KEY (ID, PID) REFERENCES works_on(ID, PID) ON DELETE CASCADE ON UPDATE CASCADE
--- );
+-- thêm comment vào equipment
+DROP PROCEDURE IF EXISTS insert_comment;
+DELIMITER \\
+CREATE PROCEDURE insert_comment(in EquipmentID int, in NID int, in comment varchar(200))
+	BEGIN
+		If (NID is null) Then
+			Begin
+				DECLARE NID_ int;
+				set NID_ = (SELECT IFNULL(MAX(Notification.NID)+1, 1)
+						FROM Notification);
+				insert into Notification(NID,PID) values(NID_,EquipmentID);
+				insert into Comments values(NID_,comment);
+            end;
+		ELSE
+            insert into Comments values(NID,comment);
+		END if;
+    END \\
+DELIMITER ;
